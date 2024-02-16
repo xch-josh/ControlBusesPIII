32e18c53c9be3d9fe8c47d27072887319de284bb
xch-josh <dannyxicay@gmail.com>
2024-02-15 20:16:42 -0600

    Manejo de los datos de regiones

--- src/Application/Data/DatosRegiones.java
+++ src/Application/Data/DatosRegiones.java
@@ -3,64 +3,59 @@ package Application.Data;
 import Application.Modelos.ModeloRegiones;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatosRegiones {
 	private final String rutaDatosRegiones = "C:/DatosControlBuses/regiones.txt";
 	
+	public DatosRegiones() throws IOException{
+		File miArchivo = new File(rutaDatosRegiones);
+		if (!miArchivo.exists()){//DETERMINA SI EL ARCHIVO ESPECIFICADO NO EXISTE
+				miArchivo.createNewFile();//CREA EL ARCHIVO EN CASO DE NO EXISTIR
+		}
+	}
+	
+	private List<ModeloRegiones> ObtenerDatos() throws Exception{
+		List<ModeloRegiones> lst = new ArrayList();
+		FileReader archivo = new FileReader(rutaDatosRegiones);
+		BufferedReader entrada = new BufferedReader(archivo);
+		String datos = entrada.readLine();
+
+		while (datos != null) {//RECORRE EL ARCHIVO MIENTRAS SE ENCUENTREN DATOS DENTRO DE ESTE
+			String[] region = datos.split("\\|");//CREA UN ARREGLO CON LA LINEA ACTUAL DE LA LECTURA DEL ARCHIVO
+			ModeloRegiones modelo = new ModeloRegiones();
+				
+			//ASIGNAMOS LOS DATOS CORRESPONDIENTES AL MODELO DE LAS REGIONES
+			modelo.setIdRegion(Integer.parseInt(region[0]));
+			modelo.setNombreRegion(region[1]);
+			modelo.setRecorrido(region[2]);
+			
+			lst.add(modelo);//AGREGAMOS EL MODELO AL LIST
+			
+			datos = entrada.readLine();
+		}
+
+		archivo.close();//SE CIERRA LA LECTURA DEL ARCHIVO
+			
+		return lst;
+	}
 	
 	private void Registrar(ModeloRegiones modelo){
-		File miArchivo = new File(rutaDatosRegiones);
-		Scanner teclado = new Scanner(System.in);
-                    String cadena, codigo, nombre, categoria;
-		float precioUnitario;
 		
-
-                    System.out.println("\n<----REGISTRAR PRODUCTO");
-                    
                     try (FileWriter fw = new FileWriter(rutaDatosRegiones, true); PrintWriter salida = new PrintWriter(fw)) {
-			if (!miArchivo.exists()){//DETERMINA SI EL ARCHIVO ESPECIFICADO NO EXISTE
-				miArchivo.createNewFile();//CREA EL ARCHIVO EN CASO DE NO EXISTIR
-			}
 			FileReader fr = new FileReader(rutaDatosRegiones);
-			BufferedReader entrada = new BufferedReader(fr);
-			String cadenaCategoria = entrada.readLine();
-			boolean hayCategoria = false;
+			String cadena = "";
 			
-			System.out.println("Ingrese el código del producto:");
-			codigo = teclado.nextLine();
-			System.out.println("Ingrese el nombre del producto:");
-			nombre = teclado.nextLine();
-			System.out.println("Ingrese la categoría del producto:");
-			categoria = teclado.nextLine();
-			System.out.println("Ingrese el precio unitario del producto:");
-			precioUnitario = teclado.nextFloat();
+			cadena = modelo.getNombreRegion().trim() + "|" + modelo.getRecorrido().trim();//CONCATENA TODOS LOS DATOS REQUERIDOS PARA EL REGISTRO EN UNA LINEA DE TEXTO SEPARANDO CADA CAMPO CON "|"
 			
-			while (cadenaCategoria != null) {//RECORRE EL ARCHIVO MIENTRAS SE ENCUENTREN DATOS DENTRO DE ESTE
-				if (cadenaCategoria.contains(categoria))
-					hayCategoria = true;
-			}
+			salida.println(cadena);
 			
-			if (!hayCategoria) {
-				int ingrCat;
-				System.out.println("La categoría no existe desea registrarla? (1=Si / 0=No)");
-				ingrCat = teclado.nextInt();
-				if (ingrCat == 1) {
-					RegistrarCategoria(categoria);
-				}else{
-					return;
-				}
-			}
-			
-			cadena = codigo.trim() + "|" + nombre.trim() + "|" + precioUnitario + "|0|" + categoria;//CONCATENA TODOS LOS DATOS REQUERIDOS PARA EL REGISTRO EN UNA LINEA DE TEXTO SEPARANDO CADA CAMPO CON "|"
-			
-			salida.println(cadena);//INSERTA LA LINEA DE TEXTO AL ARCHIVO
-			System.out.println("\nProducto guardado exitosamente\n\n");
-							  
 			salida.close();
 			fw.close();
                     } catch (IOException ex) {
